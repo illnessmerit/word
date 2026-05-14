90a0ee096f48039e58e7715fcbe8e2a498b49fb6
a <a@as-Virtual-Machine.local>
2026-05-13 18:20:07 -0700

    Append README.md to the Neovim run script invocation

--- devenv.nix
+++ devenv.nix
@@ -33,7 +33,7 @@
   '';
   scripts.release.exec = "shadow-cljs release main";
   scripts.run.exec = ''
-    nvim "+lua vim.fn.Style(1)"
+    nvim "+lua vim.fn.Style(1)" README.md
   '';
   scripts.watch.exec = ''
     nvim +star "+te tail -F node.log -n +1"
